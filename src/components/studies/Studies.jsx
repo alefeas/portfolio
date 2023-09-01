68a2ab9dec21ba6e7ffb7e81525b28b19356195b
Alejo Feas MatejÂ <alefeas99@gmail.com>
2023-09-01 09:30:17 -0300

    Update

--- src/components/studies/Studies.jsx
+++ src/components/studies/Studies.jsx
@@ -7,22 +7,26 @@ export const Studies = () => {
         {
             title: t('studies.backend-title'),
             description: t('studies.backend-description'),
-            image: 'https://res.cloudinary.com/hdsqazxtw/image/upload/v1642799651/WWW/Group_1.jpg'
+            image: 'https://res.cloudinary.com/hdsqazxtw/image/upload/v1642799651/WWW/Group_1.jpg',
+            certificateLink: '#studies'
         },
         {
             title: 'ReactJS',
             description: t('studies.react-description'),
-            image: 'https://res.cloudinary.com/hdsqazxtw/image/upload/v1642799651/WWW/Group_1.jpg'
+            image: 'https://res.cloudinary.com/hdsqazxtw/image/upload/v1642799651/WWW/Group_1.jpg',
+            certificateLink: 'https://www.coderhouse.com/certificados/6350a5b57c3cfc000fa4e97f'
         },
         {
             title: 'JavaScript',
             description: t('studies.js-description'),
-            image: 'https://res.cloudinary.com/hdsqazxtw/image/upload/v1642799651/WWW/Group_1.jpg'
+            image: 'https://res.cloudinary.com/hdsqazxtw/image/upload/v1642799651/WWW/Group_1.jpg',
+            certificateLink: 'https://www.coderhouse.com/certificados/632362bc678486000f39609a'
         },
         {
             title: t('studies.web-development-title'),
             description: t('studies.web-development-description'),
-            image: 'https://res.cloudinary.com/hdsqazxtw/image/upload/v1642799651/WWW/Group_1.jpg'
+            image: 'https://res.cloudinary.com/hdsqazxtw/image/upload/v1642799651/WWW/Group_1.jpg',
+            certificateLink: 'https://www.coderhouse.com/certificados/62c0bbeb5e6c940024b1d55a'
         }
     ]
 
@@ -32,13 +36,13 @@ export const Studies = () => {
             <div className="studies">
                 {
                     studies.map(item =>
-                        <div className="studie">
-                            <img src={item.image} alt={item.title} />
+                        <a target={item.certificateLink === '#studies' ? '' : '_blank'} rel="noreferrer" href={item.certificateLink} className="studie">
+                            <img src={item.image} alt={item.title} />   
                             <div>
                                 <span>{item.title}</span>
                                 <p>{item.description}</p>
                             </div>
-                        </div>
+                        </a>
                     )
                 }
             </div>
